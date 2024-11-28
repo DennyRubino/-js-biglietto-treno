39595b2886a1b39d8552fe6f4952063ac1c80607
Denny Rubino <rubidenny@gmail.com>
2024-11-28 14:33:00 +0100

    init project

---
+++ script.js
@@ -0,0 +1,17 @@
+//Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// -il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+
+//Chiedere il  numero dei chilometri da percorrere
+const kilometers = prompt("Quanti chilometri vuoi percorrere?");
+//chiedere l'età del passegero
+// calcolare il costo del viaggio (0.21 € al km) 
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+
+//Dare al output il prezzo in forma umana
